5eb6e680f4c83ad7471ca445986e446c371c8778
fabio-writes-code <fabiohc1@hotmail.com>
2022-08-05 22:11:36 -0600

    Revert "adds interactive UI"
    This reverts commit 2a22957c074044f303288ae5d4d75ef19ba73b87.

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.querySelector('.reset-btn').addEventListener('click',()=>reset());
-
-const btns=document.querySelectorAll('#player-selection');
-btns.forEach(btn=>btn.addEventListener('click',()=>
-    gameRound(btn.getAttribute('class'))
-));
-//gameRound()
-
 function getComputerChoice(){
     const min=0;
     const max=3;
@@ -14,16 +6,13 @@ function getComputerChoice(){
     return options[choice];
 }
 
-/* Not needed in this version
 function getPlayerSelection(){
     return prompt('rock, paper or scissors?').toLowerCase();
 }
-*/
 
 function playRound(playerSelection,computerSelection){
     let winner;
     let winnerChoice=['player','computer','tie'];
-    let resultString='';
     if(playerSelection==computerSelection){     
         winner=3;
     }else{
@@ -41,61 +30,32 @@ function playRound(playerSelection,computerSelection){
     }
     switch(winner){
         case 1:
-            resultString=`Your ${playerSelection} beats my ${computerSelection}`;
+            console.log(`Congratulations, your ${playerSelection} beats my ${computerSelection}`)
             break;
         case 2:
-            resultString=`My ${computerSelection} beats your ${playerSelection}`;
+            console.log(`I win!! my ${computerSelection} beats your ${playerSelection}`)
             break;
         case 3:
-            resultString=`Tie! We both played ${playerSelection}`;
+            console.log(`It's a tie! We both played ${playerSelection}`);
             break;
-    }
-    document.querySelector('.result').textContent=resultString;
+    } 
     return winnerChoice[winner-1];
 }
-let playerCount=0;
-let computerCount=0;
-function gameRound(playerSelection){
-    console.log(playerCount, computerCount);
-    let roundResult=playRound(playerSelection,getComputerChoice());
-    if (roundResult==='player'){
-        playerCount+=1;
-    }else if (roundResult==='computer'){
-        computerCount+=1;
-    }
-    document.querySelector('.playerCount').textContent=playerCount;
-    document.querySelector('.computerCount').textContent=computerCount;
-    
-    if (playerCount===2) winner('player');
-    if (computerCount===2) winner('computer');
+
+function gameRound(){
+    alert("Let's play some rock, paper. scissors. First one to win 3 rounds, WINS!")
+    let playerCount=0;
+    let computerCount=0;
+    do {
+        console.log(playerCount, computerCount);
+        let roundResult=playRound(getPlayerSelection(),getComputerChoice());
+        if (roundResult==='player'){
+            playerCount+=1;
+        }else if (roundResult==='computer'){
+            computerCount+=1;
+        }
+    } while (playerCount<=2 && computerCount<=2);
+    playerCount>computerCount? alert(`You win! You won 3 rounds, I won ${computerCount} rounds`):alert(`I win! I won 3 rounds, you won ${playerCount} rounds`);
 }
 
-function reset(){
-    document.querySelector('.result').textContent='';
-    document.querySelector('.playerCount').textContent=0;
-    document.querySelector('.computerCount').textContent=0;
-    playerCount=0;
-    computerCount=0;
-
-}
-function winner(whoWon){
-    const modal=document.querySelector('.modal');
-    // modal.style['visibility']='visible';
-    //Populating the Modal
-    whoWon==='player'?document.querySelector('.who-won').textContent='Congratulations! You Won!':document.querySelector('.who-won').textContent='I win!!';
-    modal.showModal();
-    let again='' 
-
-    document.querySelectorAll('#close-btn').forEach(btn=>btn.addEventListener('click',()=>{
-        btn.getAttribute('class')==='yes'?reset():null;
-        modal.close();
-        // modal.style['visibility']='hidden';
-    }));
-
-    let finalString='';
-    whoWon==='player'?finalString='Congratulations, you win!!':finalString='I win!!';
-
-    
-}
-
-
+gameRound()
