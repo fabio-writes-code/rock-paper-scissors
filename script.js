22d7a7180a3e396c2016631cb4d7b73ebd6646c3
fabio-writes-code <fabiohc1@hotmail.com>
2022-08-05 20:52:40 -0600

    changes max rounds to 5

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ function gameRound(playerSelection){
     document.querySelector('.playerCount').textContent=playerCount;
     document.querySelector('.computerCount').textContent=computerCount;
     
-    if (playerCount===2) winner('player');
-    if (computerCount===2) winner('computer');
+    if (playerCount===5) winner('player');
+    if (computerCount===5) winner('computer');
 }
 
 function reset(){
