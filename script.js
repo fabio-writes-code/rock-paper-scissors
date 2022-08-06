9cf3ff57f5a6a00fbc8f44faacbca74dff411e6d
fabio-writes-code <fabiohc1@hotmail.com>
2022-08-05 22:24:52 -0600

    Revert "adds interactive UI"
    This reverts commit 2a22957c074044f303288ae5d4d75ef19ba73b87.

--- script.js
+++ script.js
@@ -41,49 +41,21 @@ function playRound(playerSelection,computerSelection){
     } 
     return winnerChoice[winner-1];
 }
-let playerCount=0;
-let computerCount=0;
-function gameRound(playerSelection){
-    console.log(playerCount, computerCount);
-    let roundResult=playRound(playerSelection,getComputerChoice());
-    if (roundResult==='player'){
-        playerCount+=1;
-    }else if (roundResult==='computer'){
-        computerCount+=1;
-    }
-    document.querySelector('.playerCount').textContent=playerCount;
-    document.querySelector('.computerCount').textContent=computerCount;
-    
-    if (playerCount===2) winner('player');
-    if (computerCount===2) winner('computer');
+
+function gameRound(){
+    alert("Let's play some rock, paper. scissors. First one to win 3 rounds, WINS!")
+    let playerCount=0;
+    let computerCount=0;
+    do {
+        console.log(playerCount, computerCount);
+        let roundResult=playRound(getPlayerSelection(),getComputerChoice());
+        if (roundResult==='player'){
+            playerCount+=1;
+        }else if (roundResult==='computer'){
+            computerCount+=1;
+        }
+    } while (playerCount<=2 && computerCount<=2);
+    playerCount>computerCount? alert(`You win! You won 3 rounds, I won ${computerCount} rounds`):alert(`I win! I won 3 rounds, you won ${playerCount} rounds`);
 }
 
-function reset(){
-    document.querySelector('.result').textContent='';
-    document.querySelector('.playerCount').textContent=0;
-    document.querySelector('.computerCount').textContent=0;
-    playerCount=0;
-    computerCount=0;
-
-}
-function winner(whoWon){
-    const modal=document.querySelector('.modal');
-    // modal.style['visibility']='visible';
-    //Populating the Modal
-    whoWon==='player'?document.querySelector('.who-won').textContent='Congratulations! You Won!':document.querySelector('.who-won').textContent='I win!!';
-    modal.showModal();
-    let again='' 
-
-    document.querySelectorAll('#close-btn').forEach(btn=>btn.addEventListener('click',()=>{
-        btn.getAttribute('class')==='yes'?reset():null;
-        modal.close();
-        // modal.style['visibility']='hidden';
-    }));
-
-    let finalString='';
-    whoWon==='player'?finalString='Congratulations, you win!!':finalString='I win!!';
-
-    
-}
-
-
+gameRound()
